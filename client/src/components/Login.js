9a8a546a57539898f60987baba052d697912b526
Lisa Lee <lisawlee04@gmail.com>
2022-05-06 14:47:26 -0700

    The app runs on Heroku but Auth0 is not running.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -6,8 +6,8 @@ const Login = ({user}) => {
         <nav>
             <ul>
                 {/* if there is no user, bring the login. If there is, bring logout  */}
-                {!user ? (<li> <a href="http://localhost:3001/login">LOGIN</a></li>)
-                    : (<li> Hello, {user.nickname}<a href="http://localhost:3001/logout">LOGOUT</a></li>)}
+                {!user ? (<li> <a href="/login">LOGIN</a></li>)
+                    : (<li> Hello, {user.nickname}<a href="/logout">LOGOUT</a></li>)}
             </ul>
         </nav >
     )
--- server/db.sql
+++ server/db.sql
@@ -2,31 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 13.3
+-- Dumped from database version 14.2
 -- Dumped by pg_dump version 14.2
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE IF EXISTS techtonica1;
---
--- Name: techtonica1; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE techtonica1 WITH TEMPLATE = template0 ENCODING = 'UTF8' LOCALE = 'C';
-
-
-ALTER DATABASE techtonica1 OWNER TO postgres;
-
-\connect techtonica1
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -44,80 +21,123 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
--- Name: students; Type: TABLE; Schema: public; Owner: postgres
+-- Name: trips; Type: TABLE; Schema: public; Owner: lisalee
 --
 
-CREATE TABLE public.students (
+CREATE TABLE public.trips (
     id integer NOT NULL,
-    lastname character varying(255),
-    firstname character varying(255)
+    country character varying(255),
+    startdate date,
+    enddate date,
+    userid integer,
+    traveldate date
 );
 
 
-ALTER TABLE public.students OWNER TO postgres;
+ALTER TABLE public.trips OWNER TO lisalee;
 
 --
--- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+-- Name: searches_id_seq; Type: SEQUENCE; Schema: public; Owner: lisalee
 --
 
-CREATE SEQUENCE public.students_id_seq
-    AS integer
+ALTER TABLE public.trips ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.searches_id_seq
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.students_id_seq OWNER TO postgres;
-
---
--- Name: students_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
+    CACHE 1
+);
 
 
 --
--- Name: students id; Type: DEFAULT; Schema: public; Owner: postgres
+-- Name: users; Type: TABLE; Schema: public; Owner: lisalee
 --
 
-ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.students_id_seq'::regclass);
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL,
+    email character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.users OWNER TO lisalee;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: lisalee
+--
+
+ALTER TABLE public.users ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY (
+    SEQUENCE NAME public.users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1
+);
 
 
 --
--- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: postgres
+-- Data for Name: trips; Type: TABLE DATA; Schema: public; Owner: lisalee
 --
 
-COPY public.students (id, lastname, firstname) FROM stdin;
-1	Gomez	Andrea
-2	Lee	Becca
-3	Smith	Will
-4	Pond	Kate
-5	Lasso	Jamie
-7	Rodriguez	Arepa
-8	Smith	Crush
-9	Fonca	Billy
-10	Gomez	Teresa
-11	Tres	Prueba
-12	Cuatro	Prueba 
-13	Cinco	Prueba
+COPY public.trips (id, country, startdate, enddate, userid, traveldate) FROM stdin;
+3	Japan	2022-05-01	2022-05-15	2	2022-05-01
+1	South Korea	2023-01-01	2023-02-01	1	2023-01-01
+2	Taiwan	2023-02-01	2023-03-01	1	2023-02-01
+7	Spain	\N	\N	\N	2022-02-02
+8	Afghanistan	\N	\N	\N	2022-02-02
 \.
 
 
 --
--- Name: students_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: lisalee
 --
 
-SELECT pg_catalog.setval('public.students_id_seq', 13, true);
+COPY public.users (id, name, email) FROM stdin;
+1	Lisa	lisa@lisa.com
+2	John	john@john.com
+4	derek	derek@derek.com
+9	Lisa	lisawlee04@gmail.com
+\.
 
 
 --
--- Name: students students_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- Name: searches_id_seq; Type: SEQUENCE SET; Schema: public; Owner: lisalee
 --
 
-ALTER TABLE ONLY public.students
-    ADD CONSTRAINT students_pkey PRIMARY KEY (id);
+SELECT pg_catalog.setval('public.searches_id_seq', 8, true);
+
+
+--
+-- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: lisalee
+--
+
+SELECT pg_catalog.setval('public.users_id_seq', 9, true);
+
+
+--
+-- Name: trips searches_pkey; Type: CONSTRAINT; Schema: public; Owner: lisalee
+--
+
+ALTER TABLE ONLY public.trips
+    ADD CONSTRAINT searches_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: lisalee
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: trips searches_userid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: lisalee
+--
+
+ALTER TABLE ONLY public.trips
+    ADD CONSTRAINT searches_userid_fkey FOREIGN KEY (userid) REFERENCES public.users(id);
 
 
 --
