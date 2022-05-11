fe74ea63874ef0014cc2596e57c22c9da79333a7
Lisa Lee <lisawlee04@gmail.com>
2022-05-10 21:36:48 -0700

    Update country field to have a default option that is not a country and add function to formate date and time.

--- client/src/components/TripForm.js
+++ client/src/components/TripForm.js
@@ -17,17 +17,13 @@ const TripForm = (props) => {
 
     //create functions that handle the event of user input
     const handleCountryChange = (event) => {
-        // console.log(event);
         const country = event.target.value;
         setTrips((trips) => ({ ...trips, country }));
-        console.log("handle country change", trips);
     }
 
     const handleTraveldateChange = (event) => {
-        // console.log(event);
         const traveldate = event.target.value;
         setTrips((trips) => ({ ...trips, traveldate }));
-        console.log("handle traveldate", trips)
     }
 
     //A function to handle the post request
@@ -40,29 +36,30 @@ const TripForm = (props) => {
         }).then((response) => {
             return response.json()
         }).then((data) => {
-            console.log("From the post ", data);
+            // console.log("From the post ", data);
             props.addTrips(data);
         });
     }
 
     const handleOnClick = (e) => {
         // let emptyTrip = {
-        // country: "",
-        // traveldate: "",
+        //     country: null,
+        //     traveldate: null,
         // }
         e.preventDefault();
-        console.log("current trip is (obj)" + JSON.stringify(trips));
+        // console.log("current trip is (obj)" + JSON.stringify(trips));
         setTrips(trips); // set usestate for the form
         postTrips(trips); // make the post request to the db
-        // props.addTrips(trips); // sent the new trip to the parent
         // setTrips(emptyTrip); // clear the fields
     };
 
     return (
         <div>
             <form className="trip-form" onSubmit={props.getHolidays}>
-                <label for="country">Select a Country: </label>
+                <label for="country">Country: </label>
+
                 <select name="country" id="country" onChange={handleCountryChange} required>
+                    <option disabled={true} selected={true} value={null}> -- Select a Country -- </option>
                     {countries.map((country) => (
                         <option value={country["iso-3166"]}>
                             {country.country_name}, {country["iso-3166"]}
@@ -70,11 +67,11 @@ const TripForm = (props) => {
                     ))}
                 </select><br></br>
 
-                <label for="traveldate">Enter Travel Date:</label>
+                <label for="traveldate">Travel Date:</label>
                 <input type="date" name="traveldate" onChange={handleTraveldateChange} required /><br></br>
 
                 <button> Search Holidays </button>
-                
+
             </form >
             <button onClick={handleOnClick}>Add to list</button>
         </div>
