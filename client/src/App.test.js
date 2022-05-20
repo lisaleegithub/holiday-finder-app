2d46d544db8eecd98890bf1afb0cb6bb6c34a7cc
Lisa Lee <lisawlee04@gmail.com>
2022-05-20 14:49:33 -0700

    Add testing.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import ContactForm from './components/ContactForm';
+import TripForm from './components/TripForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+// First Test
+describe("App", () => {
+  test("renders App component", () => {
+    render(<App />);
+  });
+});
+
+// Second Test
+describe("ContactForm", () => {
+  test("render the ContactForm component", () => {
+    render(<ContactForm />);
+  });
+});
+
+// Third Test
+describe("TripForm", () => {
+  test("render the TripForm component", () => {
+    render(<TripForm />);
+  });
 });
